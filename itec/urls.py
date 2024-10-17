bde74329ce282c464edb36c240d48ed1d27e4ef8
Avery Howard <avery.d.howard@gmail.com>
2024-10-17 09:39:07 -0400

    Initial commit

---
+++ itec/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from django.contrib import admin
+from rest_framework import routers
+
+from itec import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
