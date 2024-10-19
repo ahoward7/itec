f4bca47abc300b84b5f37f6fb79a29110f819745
Avery Howard <avery.d.howard@gmail.com>
2024-10-18 20:15:18 -0400

    Start on transaction sheet design

--- itec/excel.py
+++ itec/excel.py
@@ -1,10 +1,8 @@
-import os
-import pandas as pd
-from openpyxl import load_workbook, Workbook
-from openpyxl.styles import Font, PatternFill
-from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill, Alignment
 from datetime import datetime
 from typing import List, Dict, Tuple
+import os
 
 
 def exportToExcel(items: List[Dict], serializer, file_name: str = 'data.xlsx', directory: str = None) -> str:
@@ -12,10 +10,16 @@ def exportToExcel(items: List[Dict], serializer, file_name: str = 'data.xlsx', d
     if not isinstance(items, list) or not all(isinstance(item, dict) for item in items):
         raise ValueError('Input must be a list of dictionaries')
 
+    # Get the fields from the meta class of the serializer
     fields = getFields(items, serializer)
 
+    # Create the workbook at the file path
     wb, file_path = createWorkbook(fields, file_name, directory)
+
+    # Apply formatting to the cells
     formatCells(wb)
+
+    # Save the workbook
     wb.save(file_path)
 
     return file_path
@@ -36,9 +40,6 @@ def getFields(items: List[Dict], serializer) -> List[Dict]:
 
 def createWorkbook(fields: List[Dict], file_name: str = 'data.xlsx', directory: str = None) -> Tuple[Workbook, str]:
     """Create a workbook from a list of dictionaries"""
-    df = pd.DataFrame(fields)
-    df.columns = df.columns.str.replace('_', ' ').str.title()
-
     if directory:
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -46,14 +47,47 @@ def createWorkbook(fields: List[Dict], file_name: str = 'data.xlsx', directory: 
     else:
         file_path = file_name
 
-    if os.path.exists(file_path):
-        wb = load_workbook(file_path)
-    else:
-        wb = Workbook()
-        ws = wb.active
+    # Create a new workbook and select the active worksheet
+    wb = Workbook()
+    ws = wb.active
 
-        for r in dataframe_to_rows(df, index=False, header=True):
-            ws.append(r)
+    # Extract the inventory headers
+    headers = list(fields[0].keys())
+
+    # Add the main title cell
+    ws.append([None] * len(headers))
+    ws.append([None] * len(headers))
+    ws.merge_cells(start_row=1, start_column=1, end_row=2, end_column=len(headers))
+    title_cell = ws.cell(row=1, column=1)
+    title_cell.value = 'Transaction Sheet'
+    title_cell.font = Font(bold=True, size=20, color='FFFFFF')
+    title_cell.fill = PatternFill(start_color='5e0f1f', end_color='5e0f1f', fill_type='solid')
+    title_cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    # Add another row for the losing and gaining accounts
+    ws.append([None] * len(headers))
+    ws.append([None] * len(headers))
+    ws.merge_cells(start_row=3, start_column=1, end_row=4, end_column=len(headers) // 2)
+    ws.merge_cells(start_row=3, start_column=len(headers) // 2 + 1, end_row=4, end_column=len(headers))
+    losing_cell = ws.cell(row=3, column=1)
+    losing_cell.value = 'Losing Account'
+    losing_cell.font = Font(bold=True, size=14, color='FFFFFF')
+    losing_cell.fill = PatternFill(start_color='864b57', end_color='864b57', fill_type='solid')
+    losing_cell.alignment = Alignment(horizontal='center', vertical='center')
+    gaining_cell = ws.cell(row=3, column=len(headers) // 2 + 1)
+    gaining_cell.value = 'Gaining Account'
+    gaining_cell.font = Font(bold=True, size=14, color='FFFFFF')
+    gaining_cell.fill = PatternFill(start_color='864b57', end_color='864b57', fill_type='solid')
+    gaining_cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    # Add the inventory headers
+    ws.append(headers)
+
+    # Add the rest of the data
+    for row_data in fields:
+        ws.append(list(row_data.values()))
+
+    wb.save(file_path)
 
     return wb, file_path
 
@@ -61,15 +95,12 @@ def createWorkbook(fields: List[Dict], file_name: str = 'data.xlsx', directory: 
 def formatCells(wb: Workbook) -> None:
     """Format the cells in the excel workbook"""
     ws = wb.active
-    header_font = Font(bold=True, color='FFFFFF')
-    header_fill = PatternFill(start_color='0000FF', end_color='0000FF', fill_type='solid')
 
-    for cell in ws[1]:
+    header_font = Font(bold=True, color='0f5e4e')
+
+    # Styling for the header row
+    for cell in ws[5]:
+        cell.value = cell.value.replace('_', ' ').title()
         cell.font = header_font
-        cell.fill = header_fill
-
-    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=1):
-        for cell in row:
-            cell.font = Font(bold=True)
 
     return
